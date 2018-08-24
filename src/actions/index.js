58efd6cbb08a9899920349aecb308011242d13f8
澤野　孝次 <kosawml@gmail.com>
2018-08-24 14:55:19 +0900

    implement reducers

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,10 +3,10 @@ export const INCREMENT = 'INCREMENT'
 export const DECREMENT = 'DECREMENT'
 
 export const increment = () => ({
-        type: 'INCREMENT'
+        type: INCREMENT
 })
 
 
 export const decrement = () => ({
-        type:'DECREMENT'
+        type:DECREMENT
 })
--- src/reducers/count.js
+++ src/reducers/count.js
@@ -1,18 +1,33 @@
-import { INCREMENT,DECREMENT } from '../actions'
+import {INCREMENT,DECREMENT} from '../actions'
 
-const initialstate ={ value:0}
+const initialstate = { value:0 }
 
 
-export default (state = initialstate,action)=>{
+export default (state=initialstate,action) => {
+    switch (action.type){
+        case INCREMENT:
+            return {value:state.value + 1}
+        case DECREMENT:
+            return {value:state.value - 1}
+        default:
+            return state
+    }
+}
+
+/*
+export default (state = initialstate,action) => {
+
+
     case INCREMENT:
         return {value:state.value+1}
     case DECREMENT:
         return {value:state.value-1}
-    default:
+    default
         return state
+
 }
 
-
+*/
 
 
 
